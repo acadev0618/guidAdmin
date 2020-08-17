d807987d7801c36afc0c1b20922679f2e18ea697
michaelbrinkworth <michaelbrinkworth@users.noreply.github.com>
2020-08-17 17:39:57 +0300

    responsive
    responsive

--- resources/views/client.blade.php
+++ resources/views/client.blade.php
@@ -1,9 +1,54 @@
 <!doctype html>
 <html>
     <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <style>
             html, body { 
                 background-color: #333; 
+            }
+            .header1{
+                color: white; 
+                text-align: center; 
+                margin-top: 165px; 
+                font-size: 65px;
+            }
+            .header2{
+                color: white;
+                font-size: 33px;
+                margin-top: 14%;
+                text-align: center;
+            }
+            .select_option{
+                height: 36px; 
+                width: 320px;
+            }
+            .button1 {
+                height: 36px;
+                color: black;
+                font-size: 16px;
+                border-radius: 8px white;
+                border-radius: 6px;
+                border: white solid 1px;
+                background-color: deepskyblue;
+            }
+            @media only screen and (max-width:500px) {
+                /* For mobile phones: */
+                .header1 {
+                    margin-top: 120px; 
+                    font-size: 50px;
+                }
+                .header2{
+                    font-size: 22px;
+                    margin-top: 52%;
+                }
+                .select_option{
+                    height: 35px; 
+                    width: 210px;
+                }
+                .button1 {
+                    height: 35px;
+                    font-size: 14px;
+                }
             }
         </style>
 
@@ -44,25 +89,15 @@
     </head>
     <body>
         <div id="startdlg">
-        <h1 style="color: white; text-align: center; margin-top: 165px; font-size: 65px;">Guide.fm for Listener</h1>
-        <p style="    color: white;
-            font-size: 33px;
-            margin-top: 14%;
-            text-align: center;">Select Channel</p>
+        <h1 class="header1">Guide.fm for Listener</h1>
+        <p class="header2">Select Channel</p>
         <div style="text-align: center;">
-            {{-- <input type="text" style="height: 27px; width: 200px;" name="" id="channel_name" placeholder="Enter channel"> --}}
-            <select style="height: 36px; width: 320px;" id="channel_name">
+            <select class="select_option" id="channel_name">
                 @foreach ($rooms as $room)
-            <option value="{{$room->room_name}}">{{$room->room_name}}</option>
+                    <option value="{{$room->room_name}}">{{$room->room_name}}</option>
                 @endforeach
             </select>
-            <button onclick="init()" style="height: 36px;
-                color: black;
-                font-size: 16px;
-                border-radius: 8px white;
-                border-radius: 6px;
-                border: white solid 1px;
-                background-color: deepskyblue;">Join
+            <button class="button1" onclick="init()">Join
             </button>
         </div>
     </div>
